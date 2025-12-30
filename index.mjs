57369bb923dafd0ed7b378b0236c3039576a0410
bonnai <Bonnai2112>
2025-12-30 22:59:04 +0100

    init commit

---
+++ index.mjs
@@ -0,0 +1,39 @@
+import * as bitcoin from "bitcoinjs-lib";
+import { Keypair } from "@solana/web3.js";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import { ethers } from "ethers";
+import { HDKey } from "@scure/bip32";
+import { generateMnemonic, mnemonicToSeed } from "@scure/bip39";
+import { wordlist } from "@scure/bip39/wordlists/english.js";
+
+// 1. Master seed
+const mnemonic = generateMnemonic(wordlist);
+const seed = await mnemonicToSeed(mnemonic);
+console.log("Master seed:", mnemonic);
+const rootKey = HDKey.fromMasterSeed(seed);
+
+// 2. Bitcoin
+const btcPath = "m/84'/0'/0'/0/0";
+const opBtcKey = rootKey.derive(btcPath);
+
+const btcAddress = bitcoin.payments.p2wpkh({
+  pubkey: opBtcKey.publicKey,
+  network: bitcoin.networks.bitcoin
+}).address;
+console.log("\nBitcoin address:", btcAddress);
+
+// 3. Ethereum
+const ethPath = "m/44'/60'/0'/0/0";
+const opEthKey = rootKey.derive(ethPath);
+
+const ethPub = opEthKey.publicKey.slice(1);
+const hash = keccak256(ethPub);
+const raw = ethers.hexlify(hash).slice(-40);
+const ethAddress = ethers.getAddress("0x" + raw);
+console.log("Ethereum address:", ethAddress);
+
+// 4. Solana
+const solPath = "m/44'/501'/0'/0'";
+const opSolHD = HDKey.fromMasterSeed(seed).derive(solPath);
+const solKeypair = Keypair.fromSeed(opSolHD.privateKey);
+console.log("Solana address:", solKeypair.publicKey.toBase58());
